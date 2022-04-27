fb7bdd079a0ce66925dda836102f1e943b94cdde
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-04-27 17:43:49 +0700

    fix manifest

--- src/app.js
+++ src/app.js
@@ -3,5 +3,7 @@ import './styles/main.css';
 import './script/components/app-bar';
 import main from './script/view/main';
 import './script/components/bottom-bar';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
 document.addEventListener('DOMContentLoaded', main);
--- src/script/components/news-item.js
+++ src/script/components/news-item.js
@@ -4,7 +4,7 @@ import logo from '../../public/images/logo.svg';
 class NewsItem extends HTMLElement {
   set news(news) {
     this._news = news;
-    this.setAttribute('class', 'card sm:basis-full md:basis-1/2 lg:basis-1/5 rounded-xl overflow-hidden shadow-md shadow-slate-600 m-3 border-1 border-slate-400');
+    this.setAttribute('class', 'card sm:basis-full md:basis-1/4 lg:basis-1/5 rounded-xl overflow-hidden shadow-md shadow-slate-600 m-3 border-1 border-slate-400');
     this.render();
   }
 
@@ -17,17 +17,13 @@ class NewsItem extends HTMLElement {
     });
   }
 
-  generateDesc(desc) {
-    return desc !== '' ? `${desc.substring(0, 200)}...` : '';
-  }
-
   render() {
     this.innerHTML = `
         <p class="absolute bg-opacity-70 bg-black p-2 rounded-tl-xl text-white text-bold">${this.generateDate(this._news.pubDate)}</p>
-        <img loading="lazy" class="w-full max-h-30" src="${this._news.thumbnail}" alt="${this._news.title}" />
+        <img class="lazyload w-full max-h-30" src="images/placeholder.png" data-src="${this._news.thumbnail}" alt="${this._news.title}" />
         <div class="p-2 bg-slate-100">
           <h1 class="text-xl font-bold">${this._news.title}</h1>
-          <p class="text-sm text-justify">${this.generateDesc(this._news.description)}</p>
+          <p class="text-sm text-justify text-ellipsis overflow-hidden">${this._news.description}...</p>
         </div>
         <a href="${this._news.link}" class="inline-block w-full text-center bg-blue-600 hover:bg-blue-900 p-4 rounded-b-xl text-white font-bold" target=”_blank” rel=”noreferrer noopener”>Baca selengkapnya
         <img src="${logo}" class="inline w-5" />
